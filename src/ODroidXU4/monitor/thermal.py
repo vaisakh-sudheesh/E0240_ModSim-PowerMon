fc123dacb2388cf5cd25dafde95abc86d5d9d680
Vaisakh <vaisakh.sudheesh@gmail.com>
2023-11-11 11:12:23 +0530

    Thermal data sampler

---
+++ src/ODroidXU4/monitor/thermal.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import fabric
+import re
+
+class ThermalSampler:
+    '''Wrapper class for thermal data sampling
+
+    More information on thermal sysfs can be found in https://docs.kernel.org/driver-api/thermal/sysfs-api.html
+    '''
+    def __init__(self, conn: fabric.Connection):
+        self.__conn__ = conn
+        self.__csv_header__ = 'therm_cpu0,therm_cpu1,therm_cpu2,therm_cpu4'
+        self.__csv_fieldcnt__ = len(self.__csv_header__.split(','))
+    
+    def header(self):
+        '''Returns header to be used for CSV file
+        '''
+        return self.__csv_header__
+
+
+    def sample_data(self) -> [str]:
+        '''Returns list of timestamp-ed(utc & local) csv strings to be recorded 
+        '''
+        with self.__conn__.cd('/sys/devices/virtual/thermal/'):
+            thermstat  = self.__conn__.run(
+                                'echo $(cat'\
+                                ' thermal_zone0/temp'\
+                                ' thermal_zone1/temp'\
+                                ' thermal_zone2/temp'\
+                                ' thermal_zone3/temp)',
+                             hide='stdout')
+        
+        
+        if thermstat:
+            cpustat_stdout = thermstat.tail('stdout',1).strip()
+            csved = re.sub(' +', ',', cpustat_stdout)
+            test_len = len(csved.split(','))
+            assert test_len == (self.__csv_fieldcnt__), \
+                                'Final length of CSV record seems to be wrong as '\
+                                +str(test_len)+\
+                                ', expected: '+str(self.__csv_fieldcnt__)
+            return csved
+        else:
+            return []
+        
+#### ==========================================================================
+#### Test Code
+if __name__ == '__main__':
+    import time
+    conn = fabric.Connection( '192.168.0.101', port=22, user='root', connect_kwargs={'password':'odroid'})
+    procstat = ThermalSampler(conn)
+    
+    csv_header = procstat.header()
+    print('CSV Header: '+csv_header)
+    ##TODO: Add assertion for testing csv formatted data of N number of fields
+
+    procstat_cpu = procstat.sample_data()
+    print('CSV Data: '+procstat.sample_data())
+    ##TODO: Add assertion for testing csv formatted data of N number of fields
+    
+    print ('Thermal Stat test completed...')
+
+#### ==========================================================================
