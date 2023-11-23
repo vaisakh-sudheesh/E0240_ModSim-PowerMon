76eec2a6800ab187a7b619a0659bbbbd421ea254
Vaisakh <vaisakh.sudheesh@gmail.com>
2023-11-23 19:15:53 +0530

    Adde command line arguments for dyn & stat coeffs
    Added big and little core specific arguments for dynamic and static power

--- simulation/gem5-src/configs/example/arm/odroid_xu4_sim.py
+++ simulation/gem5-src/configs/example/arm/odroid_xu4_sim.py
@@ -6,57 +6,16 @@ from m5.objects import MathExprPowerModel, PowerModel
 
 import fs_bigLITTLE as bL
 
-
-
-bigcore_dvfsghz_table_volts = {
-        '0.1'   : '0.9125',
-        '0.2'   : '0.9125',
-        '0.3'   : '0.9125',
-        '0.4'   : '0.9125',
-        '0.5'   : '0.9125',
-        '0.6'   : '0.9125',
-        '0.7'   : '0.9125',
-        '0.8'   : '0.9375',
-        '0.9'   : '0.9625',
-        '1.0'   : '0.9875',
-        '1.1'   : '1.0125',
-        '1.2'   : '1.0375',
-        '1.3'   : '1.0625',
-        '1.4'   : '1.1125',
-        '1.5'   : '1.1375',
-        '1.6'   : '1.175',
-        '1.7'   : '1.2125',
-        '1.8'   : '1.25',
-        '1.9'   : '1.25',
-        '2.0'   : '1.25',
-}
-
-littlecore_dvfs_table_volts = {
-        '0.2'   : '0.9375',
-        '0.3'   : '0.9375',
-        '0.4'   : '0.9375',
-        '0.5'   : '0.9375',
-        '0.6'   : '0.9375',
-        '0.7'   : '0.975',
-        '0.8'   : '1.025',
-        '0.9'   : '1.0625',
-        '1.0'   : '1.1125',
-        '1.1'   : '1.11625',
-        '1.2'   : '1.2125',
-        '1.3'   : '1.275',
-        '1.4'   : '1.275',
-    }
-
-
 class CpuPowerOn(MathExprPowerModel):
-    def __init__(self, cpu_path, **kwargs):
+    def __init__(self, cpu_path,big_dyncoeff, big_statcoeff, **kwargs):
         super(CpuPowerOn, self).__init__(**kwargs)
         # <x>A per IPC and then convert to Watt
+        print ('CpuPowerOn'+str(cpu_path)+', Dyn Coeff = '+big_dyncoeff+', Static Coeff = '+ big_statcoeff)
         self.dyn = (
-            "voltage * (1.000009 *  {}.ipc)".format(cpu_path)
+            "voltage * ({} * {}.ipc)".format(big_dyncoeff, cpu_path)
         )
         self.st = (
-            "voltage * (1.000020 * 0.001 * {}.ipc)".format(cpu_path)
+            "voltage * ({} * 0.001 * {}.ipc)".format(big_statcoeff, cpu_path)
         )
 
 
@@ -66,20 +25,47 @@ class CpuPowerOff(MathExprPowerModel):
 
 
 class CpuPowerModel(PowerModel):
-    def __init__(self, cpu_path, **kwargs):
+    def __init__(self, cpu_path, big_dyncoeff, big_statcoeff, **kwargs):
         super(CpuPowerModel, self).__init__(**kwargs)
         self.pm = [
-            CpuPowerOn(cpu_path),  # ON
+            CpuPowerOn(cpu_path, big_dyncoeff, big_statcoeff),  # ON
             CpuPowerOff(),  # CLK_GATED
             CpuPowerOff(),  # SRAM_RETENTION
             CpuPowerOff(),  # OFF
         ]
+
+def addOptions(parser):
+    parser.add_argument(
+        "--bigcore-dyn-pow-coeff",
+        type=str,
+        default="1.0",
+        help="Coefficient to be used in power model for Big Core's dynamic power",
+    )
+    parser.add_argument(
+        "--bigcore-stat-pow-coeff",
+        type=str,
+        default="1.0",
+        help="Coefficient to be used in power model for Big Core's static power",
+    )
+    parser.add_argument(
+        "--littlecore-dyn-pow-coeff",
+        type=str,
+        default="1.0",
+        help="Coefficient to be used in power model for Little Core's dynamic power",
+    )
+    parser.add_argument(
+        "--littlecore-stat-pow-coeff",
+        type=str,
+        default="1.0",
+        help="Coefficient to be used in power model for Little Core's static power",
+    )
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="ODroid-XU4 Power Model"
     )
+    addOptions(parser)
     bL.addOptions(parser)
     options = parser.parse_args()
 
@@ -88,12 +74,25 @@ def main():
 
     root = bL.build(options)
 
+    big_dynamic_powcoeff = options.bigcore_dyn_pow_coeff
+    big_static_powcoeff = options.bigcore_stat_pow_coeff
+
+    little_dynamic_powcoeff = options.littlecore_dyn_pow_coeff
+    little_static_powcoeff = options.littlecore_stat_pow_coeff
+
+    print ('ExBig type '+str(type(bL.Ex5BigCluster))+' .')
+    print ('ExLittle type '+str(type(bL.Ex5LittleCluster))+' .')
+
     # Wire up power models to the CPUs
     for cpu in root.system.descendants():
         if not isinstance(cpu, m5.objects.BaseCPU):
             continue
         cpu.power_state.default_state = "ON"
-        cpu.power_model = CpuPowerModel(cpu.path())
+        if ('ex5_big' in str(type(cpu))) :
+            cpu.power_model = CpuPowerModel(cpu.path(), big_dynamic_powcoeff, big_static_powcoeff)
+
+        if ('ex5_LITTLE' in str(type(cpu))) :
+            cpu.power_model = CpuPowerModel(cpu.path(), little_dynamic_powcoeff, little_static_powcoeff)
 
     bL.instantiate(options)
 
